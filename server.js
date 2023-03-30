0a9e06d6a0b2d4e4364ae4342ad08772865097f4
Stanislav Hanchev <stas.hanchev@gmail.com>
2023-03-30 22:27:02 +0300

    adde insert to database function

--- server.js
+++ server.js
@@ -4,6 +4,26 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
+const MongoClient = require('mongodb').MongoClient;
+const url = 'mongodb://127.0.0.1:27017/';
+const mongoClient = new MongoClient(url);
+
+async function insertOneIncidentToDb(comment, lat, lng, photo) {
+  const incident = { comment: comment, latitude: lat, longitude: lng, photo: photo }
+  try {
+      await mongoClient.connect();
+      const db = mongoClient.db('incidentBoard');
+      const collection = db.collection('incidents');
+      const result = await collection.insertOne()
+      console.log(count);
+  }catch(err) {
+      console.log(`Error: ${err}`);
+  } finally {
+      await mongoClient.close();
+      console.log("Database connection is closed");
+  }
+}
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -23,7 +43,7 @@ app.post('/api/comments', upload.single('photo'), (req, res) => {
   const { comment, lat, lng } = req.body;
   const photo = req.file ? `/uploads/${req.file.filename}` : null;
   console.log(`Comment: ${comment}, Lat: ${lat}, Lng: ${lng}, Photo: ${photo}`);
-
+  insertOneIncidentToDb(comment, lat, lng, photo);
   res.json({ comment, lat, lng, photo });
 });
 
